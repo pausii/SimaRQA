57d1175a5b8450991626e4c7a6afc425b849d585
ReCodink <muhammadraihan1607@gmail.com>
2024-06-24 00:04:40 +0700

    Update Route Transaksi Peminjaman dan Pengembalian Asset

---
+++ src/routes/borrowedReturnTransactionRoutes.js
@@ -0,0 +1,17 @@
+const express = require('express');
+
+const { getAllBorrowedReturnTransaction, getBorrowedReturnTransactionById, createBorrowedReturnTransaction, updateBorrowedReturnTransaction, exportBorrowedReturnTransactionToExcel, searchMaintenanceTransaction} = require('../controllers/borrowedReturnTransactionController');
+
+const { tokenVerified, adminOrDivision } = require('../middlewares/token');
+
+const route = express.Router();
+
+route.get('/', [tokenVerified, adminOrDivision], getAllBorrowedReturnTransaction);
+route.get('/search', [tokenVerified, adminOrDivision], searchMaintenanceTransaction);
+route.get('/:id', [tokenVerified, adminOrDivision], getBorrowedReturnTransactionById);
+route.post('/', [tokenVerified, adminOrDivision], createBorrowedReturnTransaction);
+route.put('/:id', [tokenVerified, adminOrDivision], updateBorrowedReturnTransaction);
+// route.get('/export/pdf', [tokenVerified, adminOrDivision], printAllMaintenanceReports);
+route.get('/export/excel', exportBorrowedReturnTransactionToExcel);
+
+module.exports = route;
