6e9db486fb9823f9fe3779e511af33257adef60d
ReCodink <muhammadraihan1607@gmail.com>
2024-06-24 03:00:00 +0700

    Update Routes Peminjaman dan Pengembalian Asset

--- src/routes/borrowedReturnTransactionRoutes.js
+++ src/routes/borrowedReturnTransactionRoutes.js
@@ -1,13 +1,13 @@
 const express = require('express');
 
-const { getAllBorrowedReturnTransaction, getBorrowedReturnTransactionById, createBorrowedReturnTransaction, updateBorrowedReturnTransaction, exportBorrowedReturnTransactionToExcel, searchMaintenanceTransaction} = require('../controllers/borrowedReturnTransactionController');
+const { getAllBorrowedReturnTransaction, getBorrowedReturnTransactionById, searchPeminjamanPengembalianAsset, createBorrowedReturnTransaction, updateBorrowedReturnTransaction, exportBorrowedReturnTransactionToExcel, searchMaintenanceTransaction} = require('../controllers/borrowedReturnTransactionController');
 
 const { tokenVerified, adminOrDivision } = require('../middlewares/token');
 
 const route = express.Router();
 
 route.get('/', [tokenVerified, adminOrDivision], getAllBorrowedReturnTransaction);
-route.get('/search', [tokenVerified, adminOrDivision], searchMaintenanceTransaction);
+route.get('/search', [tokenVerified, adminOrDivision], searchPeminjamanPengembalianAsset);
 route.get('/:id', [tokenVerified, adminOrDivision], getBorrowedReturnTransactionById);
 route.post('/', [tokenVerified, adminOrDivision], createBorrowedReturnTransaction);
 route.put('/:id', [tokenVerified, adminOrDivision], updateBorrowedReturnTransaction);
