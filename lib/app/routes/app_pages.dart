e24281a041b14bb7de613b41d1d3ff9fcaa6677b
pausii <1121130019@global.ac.id>
2024-06-23 14:01:12 +0700

    feat: add some routes

--- lib/app/routes/app_pages.dart
+++ lib/app/routes/app_pages.dart
@@ -1,5 +1,9 @@
 import 'package:get/get.dart';
 
+import '../modules/AssetMusholla/bindings/asset_musholla_binding.dart';
+import '../modules/AssetMusholla/views/asset_musholla_view.dart';
+import '../modules/Dashboard/bindings/dashboard_binding.dart';
+import '../modules/Dashboard/views/dashboard_view.dart';
 import '../modules/Login/bindings/login_binding.dart';
 import '../modules/Login/views/login_view.dart';
 import '../modules/home/bindings/home_binding.dart';
@@ -22,6 +26,16 @@ class AppPages {
       name: _Paths.LOGIN,
       page: () => const LoginView(),
       binding: LoginBinding(),
+    ),
+    GetPage(
+      name: _Paths.DASHBOARD,
+      page: () => const DashboardView(),
+      binding: DashboardBinding(),
+    ),
+    GetPage(
+      name: _Paths.ASSET_MUSHOLLA,
+      page: () => const AssetMushollaView(),
+      binding: AssetMushollaBinding(),
     ),
   ];
 }
--- lib/app/routes/app_routes.dart
+++ lib/app/routes/app_routes.dart
@@ -5,10 +5,14 @@ abstract class Routes {
   Routes._();
   static const HOME = _Paths.HOME;
   static const LOGIN = _Paths.LOGIN;
+  static const DASHBOARD = _Paths.DASHBOARD;
+  static const ASSET_MUSHOLLA = _Paths.ASSET_MUSHOLLA;
 }
 
 abstract class _Paths {
   _Paths._();
   static const HOME = '/home';
   static const LOGIN = '/login';
+  static const DASHBOARD = '/dashboard';
+  static const ASSET_MUSHOLLA = '/asset-musholla';
 }
