7d776998ab5b22f8ed6795856b0781b01c2831d4
ReCodink <muhammadraihan1607@gmail.com>
2024-06-27 12:24:31 +0700

    Update Routes

--- src/routes/profileRoutes.js
+++ src/routes/profileRoutes.js
@@ -1,20 +1,15 @@
 const express = require('express');
 
-const { getUserProfile, editUserProfile, changeUserPassword } = require('../controllers/profileController');
+const { getUserProfile, updateUserProfile, changeUserPassword } = require('../controllers/profileController');
 
-const { tokenVerified, onlyAdmin, forDivision } = require('../middlewares/token');
+const { tokenVerified, adminOrDivision } = require('../middlewares/token');
 
 const profileRoute = express.Router();
 
 // Profile Admin
-profileRoute.get('/profile-admin', [tokenVerified, onlyAdmin], getUserProfile);
-profileRoute.put('/profile-admin', [tokenVerified, onlyAdmin], editUserProfile);
-profileRoute.put('/change-password-admin', [tokenVerified, onlyAdmin], changeUserPassword);
-
-// Profile Division
-profileRoute.get('/profile-division', [tokenVerified, forDivision], getUserProfile);
-profileRoute.put('/profile-division', [tokenVerified, forDivision], editUserProfile);
-profileRoute.put('/change-password-division', [tokenVerified, forDivision], changeUserPassword);
+profileRoute.get('/', [tokenVerified, adminOrDivision], getUserProfile);
+profileRoute.put('/', [tokenVerified, adminOrDivision], updateUserProfile);
+profileRoute.put('/change-password', [tokenVerified, adminOrDivision], changeUserPassword);
 
 
 module.exports = profileRoute;
