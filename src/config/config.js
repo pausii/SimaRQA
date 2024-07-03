680d9810c8db2aabe5eaabd65f749812ab9736a4
ReCodink <muhammadraihan1607@gmail.com>
2024-07-03 21:37:47 +0700

    Update Config for add timezone

--- src/config/config.js
+++ src/config/config.js
@@ -6,20 +6,23 @@ module.exports = {
     password: process.env.DEVELOPMENT_PASSWORD,
     database: process.env.DEVELOPMENT_DATABASE,
     host: 'localhost',
-    dialect: 'mysql'
+    dialect: 'mysql',
+    timezone: '+07:00' // Use GMT+7 timezone offset
   },
   test: {
     username: process.env.TEST_USERNAME,
     password: process.env.TEST_PASSWORD,
     database: process.env.TEST_DATABASE,
     host: 'localhost',
-    dialect: 'mysql'
+    dialect: 'mysql',
+    timezone: '+07:00' // Use GMT+7 timezone offset
   },
   production: {
     username: process.env.PRODUCTION_USERNAME,
     password: process.env.PRODUCTION_PASSWORD,
     database: process.env.PRODUCTION_DATABASE,
     host: 'localhost',
-    dialect: 'mysql'
+    dialect: 'mysql',
+    timezone: '+07:00' // Use GMT+7 timezone offset
   }
 };
