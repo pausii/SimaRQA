0ee4d791417a3e76e8d35393c36edec5ff76977e
ReCodink <muhammadraihan1607@gmail.com>
2024-07-03 21:38:40 +0700

    Update Seeders Peminjaman Pengembalian Aset

--- src/seeders/20240621201658-peminjaman-pengembalian-asset-seeders.js
+++ src/seeders/20240621201658-peminjaman-pengembalian-asset-seeders.js
@@ -2,15 +2,14 @@
 
 /** @type {import('sequelize-cli').Migration} */
 module.exports = {
-  async up (queryInterface, Sequelize) {
+  async up(queryInterface, Sequelize) {
     await queryInterface.bulkInsert('peminjaman_pengembalian_asset', [
       {
         borrowed_asset_code: 'AUD004',
         borrowed_asset_name: 'Sound System 1',
         borrowed_name: 'Hana',
         used_by_program: 'MIQA',
-        borrowed_date: new Date('2024-06-01'),
-        due_date: new Date('2024-06-15'),
+        due_date: new Date('2024-06-15T12:00:00'),    // Contoh: Tambahkan jam 12:00:00
         status: 'Dikembalikan',
         notes: 'Digunakan untuk Acara Outdoor MIQA.',
         createdAt: new Date(),
@@ -21,18 +20,17 @@ module.exports = {
         borrowed_asset_name: 'Projector 1',
         borrowed_name: 'Diana',
         used_by_program: 'MIQA',
-        borrowed_date: new Date('2024-06-01'),
-        due_date: new Date('2024-06-15'),
+        due_date: new Date('2024-06-15T14:45:00'),    // Contoh: Tambahkan jam 14:45:00
         return_date: null,
         status: 'Dikembalikan',
         notes: 'Digunakan untuk presentasi MIQA.',
         createdAt: new Date(),
         updatedAt: new Date()
       },
-    ])
+    ]);
   },
 
-  async down (queryInterface, Sequelize) {
+  async down(queryInterface, Sequelize) {
     await queryInterface.bulkDelete('peminjaman_pengembalian_asset', null, {});
   }
 };
