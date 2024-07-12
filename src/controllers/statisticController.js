d311c3dfb0816e9df6f14a7d91b0ef07cae70769
ReCodink <muhammadraihan1607@gmail.com>
2024-07-12 23:47:51 +0700

    Update Statistic Controllers

--- src/controllers/statisticController.js
+++ src/controllers/statisticController.js
@@ -6,45 +6,26 @@ const handleError = (res, error, message = 'Kesalahan Server Internal', status =
 };
 
 const getCountData = async (req, res) => {
-    const { type } = req.query;
-
-    console.log('Received type:', type); // Debugging statement
-
     try {
-        let count;
+        const auditorium = await RuangAsetAuditorium.count();
+        const musholla = await RuangAsetMusholla.count();
+        const perpustakaan = await RuangAsetPerpustakaan.count();
+        const utilitas = await RuangAsetUtilitas.count();
+        const borrowedReturn = await PeminjamanPengembalianAsset.count();
+        const maintenance = await PemeliharaanAsset.count();
+        const users = await Users.count();
+        const categories = await CategoryAsset.count();
 
-        switch (type) {
-            case 'auditorium':
-                count = await RuangAsetAuditorium.count();
-                break;
-            case 'musholla':
-                count = await RuangAsetMusholla.count();
-                break;
-            case 'perpustakaan':
-                count = await RuangAsetPerpustakaan.count();
-                break;
-            case 'utilitas':
-                count = await RuangAsetUtilitas.count();
-                break;
-            case 'category':
-                count = await CategoryAsset.count();
-                break;
-            case 'users': 
-                count = await Users.count();
-                break;
-            case 'maintenance':
-                count = await PemeliharaanAsset.count();
-                break;
-            case 'borrowed-return':
-                count = await PeminjamanPengembalianAsset.count();
-                break;
-            default:
-                return res.status(400).json({ error: 'Kesalahan Parameter Tipe' });
-        }
-
-        console.log('Count:', count); // Debugging statement
-
-        res.status(200).json({ type, count });
+        res.status(200).json({
+            auditorium,
+            musholla,
+            perpustakaan,
+            utilitas,
+            borrowedReturn,
+            maintenance,
+            users,
+            categories
+        });
     } catch (error) {
         handleError(res, error);
     }
--- src/routes/statisticRoutes.js
+++ src/routes/statisticRoutes.js
@@ -5,6 +5,6 @@ const { tokenVerified, adminOrDivision } = require('../middlewares/token');
 
 const route = express.Router();
 
-route.get('/count', [tokenVerified, adminOrDivision], getCountData);
+route.get('/', [tokenVerified, adminOrDivision], getCountData);
 
 module.exports = route;
