97ff5408a7ee0854438ecc9f4e568a9d1a2af2bd
pausii <1121130019@global.ac.id>
2024-07-14 14:20:41 +0700

    fix(GenerateAssetCode): resolve duplicate errors

--- src/services/generateAssetCode.js
+++ src/services/generateAssetCode.js
@@ -1,8 +1,15 @@
 const generateAssetCode = async (Model, prefix) => {
-    const assetCount = await Model.count();
-    const newCodeNumber = assetCount + 1;
-    const assetCode = `${prefix.toUpperCase()}${newCodeNumber.toString().padStart(3, '0')}`;
-    return assetCode;
+    const lastAsset = await Model.findOne({
+        order: [['asset_id', 'DESC']]
+    });
+    
+    if (!lastAsset) {
+        return `${prefix.toUpperCase()}001`;
+    }
+
+    const codeNumber = parseInt(lastAsset.asset_code.replace(prefix, ''));
+    const newCodeNumber = codeNumber + 1;
+    return `${prefix.toUpperCase()}${newCodeNumber.toString().padStart(3, '0')}`;
 };
 
 module.exports = generateAssetCode;
